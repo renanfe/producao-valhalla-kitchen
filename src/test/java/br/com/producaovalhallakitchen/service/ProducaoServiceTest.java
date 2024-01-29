dab55c35c4251def51595a1a242b0c95a7ae62d5
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-01-29 19:58:10 -0300

    Criado classes de teste

---
+++ src/test/java/br/com/producaovalhallakitchen/service/ProducaoServiceTest.java
@@ -0,0 +1,31 @@
+package br.com.producaovalhallakitchen.service;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProducaoServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void quandoReceboUmPedido_entaoDevoProcessarOPedidoESalvar(){
+        Assert.fail("Teste não implementado");
+    }
+
+    @Test
+    public void quandoAtualizoOPedido_entaoDevoAtualizarOPedidoESalvar(){
+        Assert.fail("Teste não implementado");
+    }
+
+    @Test
+    public void quandoBuscoUmPedido_entaoDevoRetornarOPedido(){
+        Assert.fail("Teste não implementado");
+    }
+
+}
