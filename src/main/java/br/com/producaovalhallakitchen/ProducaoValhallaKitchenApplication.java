501097ebb6e2c71bac8317c330da97948d6180d6
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-01-29 19:34:51 -0300

    Estrutura e configurações iniciais

---
+++ src/main/java/br/com/producaovalhallakitchen/ProducaoValhallaKitchenApplication.java
@@ -0,0 +1,11 @@
+package br.com.producaovalhallakitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducaoValhallaKitchenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducaoValhallaKitchenApplication.class, args);
+    }
+}
