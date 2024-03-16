d2551a226dc37806c8719f02e172a57fc236fe5c
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-15 23:34:44 -0300

    Refatora classe pedido entity para mongodb

--- src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/entity/PedidoEntity.java
+++ src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/entity/PedidoEntity.java
@@ -1,42 +1,38 @@
 package br.com.producaovalhallakitchen.adapter.driven.infra.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 import java.util.UUID;
-
-@Entity
-@Table(name = "pedido")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-@SequenceGenerator(name = "pedido_generator", sequenceName = "pedido_id_seq", allocationSize = 1)
+@Getter
+@Setter
+@Document("pedido")
 public class PedidoEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pedido_generator")
-    @Column(name = "id")
-    private Long id;
 
-    @Column(name = "clienteId")
+    public PedidoEntity (String id, UUID clienteId, String nomeCliente, String status, List<Long> produtos, String statusPagamento) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.nomeCliente = nomeCliente;
+        this.status = status;
+        this.produtos = produtos;
+        this.statusPagamento = statusPagamento;
+    }
+
+    @Id
+    private String id;
+
     private UUID clienteId;
 
-    @Column(name = "nome_cliente")
     private String nomeCliente;
 
-    @Column(name = "status")
-    @Builder.Default
-    @Setter
     private String status = "Recebido";
 
-    @ElementCollection
-    @CollectionTable(name = "pedido_produto",
-            joinColumns = @JoinColumn(name = "pedido_id"))
-    @Column(name = "produto_id")
     private List<Long> produtos;
 
-    @Column(name = "status_pagamento")
-    @Builder.Default
     private String statusPagamento = "Aguardando";
 }
