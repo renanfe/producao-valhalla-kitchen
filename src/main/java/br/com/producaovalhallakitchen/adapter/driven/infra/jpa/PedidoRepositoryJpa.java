7d0bfa45b8019abf98650e4930ab89f0486075d3
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-15 23:36:07 -0300

    Alterado JpaRepository para MongoRepository e de Long para String

--- src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/jpa/PedidoRepositoryJpa.java
+++ src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/jpa/PedidoRepositoryJpa.java
@@ -1,11 +1,12 @@
 package br.com.producaovalhallakitchen.adapter.driven.infra.jpa;
 
 import br.com.producaovalhallakitchen.adapter.driven.infra.entity.PedidoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface PedidoRepositoryJpa extends JpaRepository<PedidoEntity, Long> {
+public interface PedidoRepositoryJpa extends MongoRepository<PedidoEntity, String> {
 
     List<PedidoEntity> findByStatusIn(List<String> status);
 
