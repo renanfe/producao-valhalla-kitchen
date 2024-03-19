350b08115ae9b3b2aca20c96f807b89630657851
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 22:24:51 -0300

    criado teste implementacao sqs out

---
+++ src/test/java/br/com/producaovalhallakitchen/messaging/PedidoSQSOUTImplTest.java
@@ -0,0 +1,33 @@
+package br.com.producaovalhallakitchen.messaging;
+
+import br.com.producaovalhallakitchen.adapter.driven.infra.messaging.PedidoSQSOUTImpl;
+import br.com.producaovalhallakitchen.core.applications.ports.PedidoSQSOUT;
+import br.com.producaovalhallakitchen.utils.PedidoHelper;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PedidoSQSOUTImplTest {
+
+    private PedidoSQSOUT pedidoSQSOUT;
+
+    @Mock
+    private SqsTemplate sqsTemplate;
+
+    @BeforeEach
+    void setUp() {
+        this.pedidoSQSOUT = new PedidoSQSOUTImpl(sqsTemplate);
+    }
+
+    @Test
+    void quandoAlteraStatusDoPedido_entaoDevoEncaminharMensagemComOPedido(){
+        doReturn(PedidoHelper.buildSendResult()).when(sqsTemplate).send(any());
+        this.pedidoSQSOUT.publicarAtualizacaoPedido(PedidoHelper.buildPedido());
+        verify(this.sqsTemplate, times(1)).send(any());
+    }
+}
