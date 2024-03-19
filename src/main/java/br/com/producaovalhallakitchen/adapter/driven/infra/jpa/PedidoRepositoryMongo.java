1f5e3c7b05e6aaaec4b9891af5a22f34ff9ec6d3
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 23:38:19 -0300

    Alterado para receber Status no param do metodo

--- src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/jpa/PedidoRepositoryMongo.java
+++ src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/jpa/PedidoRepositoryMongo.java
@@ -1,12 +1,13 @@
 package br.com.producaovalhallakitchen.adapter.driven.infra.jpa;
 
 import br.com.producaovalhallakitchen.adapter.driven.infra.entity.PedidoEntity;
+import br.com.producaovalhallakitchen.core.domain.Status;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
 public interface PedidoRepositoryMongo extends MongoRepository<PedidoEntity, String> {
 
-    List<PedidoEntity> findByStatusIn(List<String> status);
+    List<PedidoEntity> findByStatusIn(List<Status> status);
 
 }
