4f9886801d6b932b52c7f05ee301a3e6c6a75ec7
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 22:24:30 -0300

    criado implementacao sqs out

---
+++ src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/messaging/PedidoSQSOUTImpl.java
@@ -0,0 +1,30 @@
+package br.com.producaovalhallakitchen.adapter.driven.infra.messaging;
+
+import br.com.producaovalhallakitchen.core.applications.ports.PedidoSQSOUT;
+import br.com.producaovalhallakitchen.core.domain.Pedido;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoSQSOUTImpl implements PedidoSQSOUT {
+
+    private SqsTemplate sqsTemplate;
+
+    @Value("${queue.atualizacao-situacao-pedido}")
+    private String queueAtualizacaoSituacaoPedido;
+
+    public PedidoSQSOUTImpl(SqsTemplate sqsTemplate) {
+        this.sqsTemplate = sqsTemplate;
+    }
+
+    @Override
+    public void publicarAtualizacaoPedido(Pedido pedido) {
+        this.sqsTemplate
+                .send(sqsSendOptions ->
+                        sqsSendOptions
+                                .queue(queueAtualizacaoSituacaoPedido)
+                                .payload(pedido)
+                );
+    }
+}
