ce30348a72d95398665434a4dc84605e361e5715
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 22:30:17 -0300

    adcionado final na variavel PedidoService

--- src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/messaging/PedidoSQSINImpl.java
+++ src/main/java/br/com/producaovalhallakitchen/adapter/driven/infra/messaging/PedidoSQSINImpl.java
@@ -3,14 +3,13 @@ package br.com.producaovalhallakitchen.adapter.driven.infra.messaging;
 import br.com.producaovalhallakitchen.adapter.driven.infra.ports.PedidoService;
 import br.com.producaovalhallakitchen.adapter.driver.form.PedidoForm;
 import br.com.producaovalhallakitchen.core.applications.ports.PedidoSQSIN;
-import br.com.producaovalhallakitchen.core.applications.services.PedidoServiceImpl;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PedidoSQSINImpl implements PedidoSQSIN {
-    private PedidoService pedidoService;
+    private final PedidoService pedidoService;
 
     public PedidoSQSINImpl(PedidoService pedidoService) {
         this.pedidoService = pedidoService;
